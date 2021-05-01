9cb2e1a35d5ad2974c8e4e3f8017f553367ab47e
Money014 <wanchana0014@gmail.com>
2021-05-02 00:24:46 +0700

    Add "That Wrong" Under FistLogin AND Change Username And Password

--- Exercise9-Wanchana-P.py
+++ Exercise9-Wanchana-P.py
@@ -1,11 +1,13 @@
 print("--------------------------------")
 UserNameInput = input("UserName : ")
 PasswordInput = input("PassWord : ")
-while UserNameInput != "admin" or PasswordInput != "1234":
+if UserNameInput != "I LOVE U" or PasswordInput != "3000":
+    print("That's Wrong")
+while UserNameInput != "I LOVE U" or PasswordInput != "3000":
     print("--------------------------------")
     UserNameInput = input("UserName : ")
     PasswordInput = input("PassWord : ")
-    if UserNameInput != "admin" or PasswordInput != "1234":
+    if UserNameInput != "I LOVE U" or PasswordInput != "3000":
         print("That's Wrong")
 print("Done")
 print("--------------------------------")
